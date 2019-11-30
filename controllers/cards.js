c109839e4bbf18c717acca10535d0ae130813693
Sergei Grobov <sgrobov@yandex-team.ru>
2019-11-30 14:13:13 +0500

    Controllers logic

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
+
 const BaseController = require('./base');
 
 class CardsController extends BaseController {
-  getCards() {
-    this.sendJson({});
+  async getCards() {
+    try {
+      const fileData = await fs.promises.readFile('data/cards.json', 'utf8');
+
+      this.sendJson(JSON.parse(fileData));
+    } catch (err) {
+      console.error(err);
+
+      this.sendJson('Internal Error', 500);
+    }
   }
 }
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,41 @@
+const fs = require('fs');
+
 const BaseController = require('./base');
 
+function readUsersFile() {
+  return fs.promises.readFile('data/users.json', 'utf8');
+}
+
 class UsersController extends BaseController {
-  getUsers() {
-    this.sendJson({});
+  async getUsers() {
+    try {
+      const fileData = await readUsersFile();
+
+      this.sendJson(JSON.parse(fileData));
+    } catch (err) {
+      console.error(err);
+
+      this.sendJson('Internal Error', 500);
+    }
   }
 
-  getUser() {
-    this.sendJson({});
+  async getUser() {
+    try {
+      const fileData = await readUsersFile();
+      const allUsers = JSON.parse(fileData);
+      const { id } = this.req.params;
+      const user = allUsers.find((item) => item._id === id);
+
+      if (!user) {
+        return this.sendJson({ message: 'Нет пользователя с таким id' }, 404);
+      }
+
+      this.sendJson(user);
+    } catch (err) {
+      console.error(err);
+
+      this.sendJson('Internal Error', 500);
+    }
   }
 }
 
